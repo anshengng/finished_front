aa73c246fa27d4b336048d54c38ff54a3b41496a
LiuKun <13655914562@163.com>
2023-12-01 23:00:33 +0800

    ud globalcss

--- src/components/Top/topBar.vue
+++ src/components/Top/topBar.vue
@@ -6,7 +6,7 @@
 <template>
     <!-- width 63% -->
     <div class="w-[63%] h-full bg-cyan-200 flex items-center">
-        <div>icon</div>
+        <div >icon</div>
         <div class="w-full">
             <ul class="flex gap-20 justify-center">
                 <li>group 1</li>
