39164938d86aec4660c1fccc53caf1d3ec88dacf
LiuKun <13655914562@163.com>
2024-01-03 22:58:57 +0800

    Update topBar.vue

--- src/components/Top/topBar.vue
+++ src/components/Top/topBar.vue
@@ -7,7 +7,7 @@ const res = await getCategory()
 
 <template>
     <!-- width 63% -->
-    <div class="w-[63%] h-full bg-cyan-200 flex items-center">
+    <div class="w-[63%] h-full bg-white flex items-center">
         <div >icon</div>
         <div class="w-full">
             <ul class="flex gap-20 justify-center">
