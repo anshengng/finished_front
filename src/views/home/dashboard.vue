9243275c2c01d366cfb55ccb6852b68e9f2a3e01
LiuKun <13655914562@163.com>
2024-01-04 22:24:16 +0800

    Update dashboard.vue

--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -11,26 +11,20 @@ import 'swiper/css/pagination';
 
 
 // import required modules
-import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
-const modules = [Keyboard, Scrollbar, Navigation, Pagination]
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+const modules = [EffectFade, Navigation, Pagination]
+
 </script>
 
 
 <template>
-    <div class="swiper w-screen h-[550px] bg-cyan-400">
-        <swiper :slidesPerView="2" :centeredSlides="false" :slidesPerGroupSkip="1" :grabCursor="true" :keyboard="{
-            enabled: true,
-        }" :breakpoints="{
-    '769': {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-    },
-}" :scrollbar="true" :navigation="true" :pagination="{
-    clickable: true,
-}" :modules="modules" class="mySwiper ">
-            <swiper-slide class="object-contain"><img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg" /></swiper-slide>
-            <swiper-slide class="object-contain"><img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg" /></swiper-slide>
-            <swiper-slide><img src="/images/swiper1.jpg" /></swiper-slide>
+    <div class="swiper w-screen h-[700px] bg-cyan-400">
+        <swiper :spaceBetween="30" :effect="'fade'" :navigation="true" :pagination="{
+            clickable: true,
+        }" :modules="modules" class="mySwiper">
+            <swiper-slide class="z-1"><img class="swiper-item" src="https://swiperjs.com/demos/images/nature-1.jpg" /></swiper-slide>
+            <swiper-slide><img class="swiper-item" src="https://swiperjs.com/demos/images/nature-2.jpg" /></swiper-slide>
+            <swiper-slide><img class="swiper-item" src="/images/swiper1.jpg" /></swiper-slide>
         </swiper>
     </div>
     <main class="relative w-[63%] min-h-[1500px] bg-pink-300">
@@ -45,61 +39,12 @@ const modules = [Keyboard, Scrollbar, Navigation, Pagination]
 
 
 <style lang="scss" scoped>
-.swiper {
-    width: 100%;
-    height: 100%;
-}
-
-.swiper-slide {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-}
-
-.swiper-slide img {
-    display: block;
-    width: 100%;
-}
-
-@media only screen and (min-width: 769px) {
-    .swiper-slide:first-child {
-        transition: transform 100ms;
-    }
-
-    .swiper-slide:first-child img {
-        transition: box-shadow 500ms;
-    }
-
-    .swiper-slide.swiper-slide-active:first-child {
-        transform: translateX(50%);
-        z-index: 2;
-    }
-
-    .swiper-slide.swiper-slide-active:first-child img {
-        box-shadow: 0px 32px 80px rgba(0, 0, 0, 0.35);
-    }
-
-    .swiper-slide:nth-child(2) {
-        transition: transform 100ms;
-    }
-
-    .swiper-slide.swiper-slide-next:nth-child(2) {
-        transform: translateX(55%);
-        z-index: 1;
-    }
-
-    .swiper[dir="rtl"] .swiper-slide.swiper-slide-active:first-child {
-        transform: translateX(-50%);
-    }
-
-    .swiper[dir="rtl"] .swiper-slide.swiper-slide-next:nth-child(2) {
-        transform: translateX(-55%);
-    }
-}
-
 .mySwiper {
-    @apply h-[600px]
+    @apply h-full pr-3;
 }
+.swiper-item {
+    z-index: 1;
+    @apply h-full object-cover;
+}
+
 </style>
