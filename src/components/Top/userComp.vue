9797c14ffa54086cdc96f43a21b9cf3eade38e6d
LiuKun <13655914562@163.com>
2024-01-12 22:35:50 +0800

    Update userComp.vue

--- src/components/Top/userComp.vue
+++ src/components/Top/userComp.vue
@@ -5,8 +5,8 @@
 
 <template>
     <div class="flex gap-2">
-        <button @click="$router.push('/login')" class="bg-slate-300 rounded-md py-1 px-1 w-12 h-8 cursor-pointer hover:text-white hover:bg-slate-500">登录</button>
-        <button @click="$router.push('/register')" class="bg-slate-300 rounded-md py-1 px-1 w-12 h-8 cursor-pointer hover:text-white hover:bg-slate-500">注册</button>
+        <button @click="$router.push('/login')" class="bg-slate-300 rounded-md py-1 px-1 w-12 h-8 cursor-pointer hover:text-white hover:bg-slate-500">登录 </button>
+        <button @click="$router.push('/register')" class="bg-slate-300 rounded-md py-1 px-1 w-12 h-8 cursor-pointer hover:text-white hover:bg-slate-500">注册 </button>
     </div>
 </template>
 
